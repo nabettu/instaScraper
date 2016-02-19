f29da5805d0fc43ce04ec1df1516ec2912908e49
nabettu <tatsuaki.w@gmail.com>
2016-02-19 16:39:48 +0900

    変数でなくobjectを渡すことに

--- index.php
+++ index.php
@@ -10,8 +10,12 @@ curl_setopt($cp, CURLOPT_TIMEOUT, 60);
 $contents = curl_exec($cp);
 curl_close($cp);
 
+$varStr = "window._sharedData = ";
 $content = mb_convert_encoding($contents, 'UTF-8', 'auto');
-$strStart = substr($content, strpos($content, 'window._sharedData'));
-$data = substr($strStart, 0, strpos($strStart, '<script'));
-echo '<script>'.$data;
+$strStart = substr($content, strpos($content, $varStr) + strlen($varStr));
+
+$closetag = "</script>";
+$data = substr($strStart, 0, strpos($strStart, '<script') - strlen($closetag) - 1);
+echo $data;
 exit();
+?>
